d3b0a85ee91c4c23f9e960d51350e6b05a575832
Vahan Sosoyan <sosoyan@gmail.com>
2018-09-01 19:57:35 +0930

    aton_enabled on

--- scripts/aton_houdini.py
+++ scripts/aton_houdini.py
@@ -677,7 +677,7 @@ class Aton(QtWidgets.QWidget):
             
             # Aton Attributes
             userOptions += ' ' if userOptions else ''
-            userOptions += 'declare aton_enable constant BOOL aton_enable off '
+            userOptions += 'declare aton_enable constant BOOL aton_enable on '
             userOptions += 'declare aton_host constant STRING aton_host %s '%self.hostLineEdit.text()
             userOptions += 'declare aton_port constant INT aton_port %d '%self.portSlider.value()
             userOptions += 'declare aton_output constant STRING aton_output %s '%self.output.name
